02005caca9dbd780a6fe2364ed53b69ee90d4520
Rao <GRao9@its.jnj.com>
2023-12-03 08:21:29 -0500

    Add Authorize functions

---
+++ R/AuthorizeToAtlas.R
@@ -0,0 +1,25 @@
+
+
+authorizeToOhdsiPlAtlas <- function(keyringName = "ohda") {
+  baseUrl <- "https://atlas-phenotype.ohdsi.org/WebAPI"
+  return(
+    ROhdsiWebApi::authorizeWebApi(
+      baseUrl = baseUrl,
+      authMethod = "db",
+      webApiUsername = keyring::key_get(service = "ohdsiAtlasPhenotypeUser",
+                                        keyring = keyringName),
+      webApiPassword = keyring::key_get(service = "ohdsiAtlasPhenotypePassword",
+                                        keyring = keyringName)
+    )
+  )
+}
+
+authorizedToJnjAtlas <- function() {
+  baseUrl <- Sys.getenv("baseUrl")
+  ROhdsiWebApi::authorizeWebApi(
+    baseUrl = baseUrl,
+    authMethod = "windows",
+    keyring::key_get(service = "ADMIN_USER", keyring = keyringName),
+    keyring::key_get(service = "ADMIN_PASSWORD", keyring = keyringName)
+  ) # Windows authentication
+}
