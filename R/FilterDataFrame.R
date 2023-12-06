cb99ee61f2e56cd54aeb3b00be167e5604a08f2a
Gowtham Rao <rao@ohdsi.org>
2023-12-06 16:16:18 -0500

    Update FilterDataFrame.R

--- R/FilterDataFrame.R
+++ R/FilterDataFrame.R
@@ -1,25 +1,39 @@
 #' @export
 filterDataFrame <- function(data, ...) {
-  # Load necessary libraries
-  rlang::check_installed("dplyr", dependencies = TRUE)
+  # Check if 'dplyr' is installed
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("The 'dplyr' package is not installed. Please install it to use this function.")
+  }
   
   # Capture the filtering conditions
   conditions <- rlang::quos(...)
   
   # Check for missing variables and incompatible data types
   for (cond in conditions) {
-    # Extract variable name and value from the condition
-    varName <- all.vars(cond[[2]])[1]
-    value <- eval(cond[[2]][[3]])
+    # Extract the expression
+    expr <- rlang::quo_get_expr(cond)
+    
+    # Extract variable name
+    if (length(expr) < 3) {
+      stop("Invalid condition: ", rlang::quo_text(cond))
+    }
+    varName <- all.vars(expr)[1]
     
     # Check if variable exists in the data frame
     if (!varName %in% names(data)) {
       stop("Variable '", varName, "' not found in the data frame.")
     }
     
+    # Evaluate the right-hand side of the expression in the context of the data frame
+    rhsValue <- tryCatch({
+      eval(expr[[3]], envir = data)
+    }, error = function(e) {
+      stop("Error evaluating the condition '", rlang::quo_text(cond), "': ", e$message)
+    })
+    
     # Check data type compatibility
-    if (!is.na(value) && !inherits(value, class(data[[varName]]))) {
-      stop("Incompatible data types for variable '", varName, "'. Expected ", class(data[[varName]]), ", got ", class(value), ".")
+    if (!is.na(rhsValue) && !inherits(rhsValue, class(data[[varName]]))) {
+      stop("Incompatible data types for variable '", varName, "'. Expected ", class(data[[varName]]), ", got ", class(rhsValue), ".")
     }
   }
   
