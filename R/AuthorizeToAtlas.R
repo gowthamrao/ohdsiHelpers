a39ad7116be7f0a029ad898d8f806ae36b99034e
Gowtham Rao <rao@ohdsi.org>
2023-12-09 05:25:45 -0500

    t

--- R/AuthorizeToAtlas.R
+++ R/AuthorizeToAtlas.R
@@ -1,18 +1,15 @@
 #' @export
 authorizeToOhdsiPlAtlas <- function(keyringName = "ohda") {
   baseUrl <- "https://atlas-phenotype.ohdsi.org/WebAPI"
+  message(paste("Authorizing to ", baseUrl))
   return(
-    ROhdsiWebApi::authorizeWebApi(
+    authorizeToWebApi(
       baseUrl = baseUrl,
       authMethod = "db",
-      webApiUsername = keyring::key_get(
-        service = "ohdsiAtlasPhenotypeUser",
-        keyring = keyringName
-      ),
-      webApiPassword = keyring::key_get(
-        service = "ohdsiAtlasPhenotypePassword",
-        keyring = keyringName
-      )
+      webApiUsername = keyring::key_get(service = "ohdsiAtlasPhenotypeUser",
+                                        keyring = keyringName),
+      webApiPassword = keyring::key_get(service = "ohdsiAtlasPhenotypePassword",
+                                        keyring = keyringName)
     )
   )
 }
@@ -20,10 +17,61 @@ authorizeToOhdsiPlAtlas <- function(keyringName = "ohda") {
 #' @export
 authorizeToJnjAtlas <- function(keyringName = "ohda") {
   baseUrl <- Sys.getenv("baseUrl")
-  ROhdsiWebApi::authorizeWebApi(
-    baseUrl = baseUrl,
-    authMethod = "windows",
-    keyring::key_get(service = "ADMIN_USER", keyring = keyringName),
-    keyring::key_get(service = "ADMIN_PASSWORD", keyring = keyringName)
-  ) # Windows authentication
+  message(paste("Authorizing to ", baseUrl))
+  
+  return(
+    authorizeToWebApi(
+      baseUrl = Sys.getenv("baseUrl"),
+      authMethod = "windows",
+      webApiUsername = keyring::key_get(service = "ADMIN_USER", keyring = keyringName),
+      webApiPassword = keyring::key_get(service = "ADMIN_PASSWORD", keyring = keyringName)
+    )
+  )# Windows authentication
+}
+
+#' @export
+authorizeToWebApi <-
+  function(baseUrl,
+           authMethod,
+           webApiUsername,
+           webApiPassword) {
+    process <-   ROhdsiWebApi::authorizeWebApi(
+      baseUrl = baseUrl,
+      authMethod = authMethod,
+      webApiUsername = webApiUsername,
+      webApiPassword = webApiPassword
+    )
+    return(process)
+  }
+
+attemptProcess <- function(process,
+                           iteration = 5,
+                           waitFor = 10) {
+  result <- try({
+    process()
+  }, silent = TRUE)
+  
+  if (class(result) == "try-error") {
+    return(FALSE)
+  } else {
+    return(result)
+  }
+  
+  for (i in 1:iteration) {
+    message(paste("Attempt", i, "of ", iteration))
+    result <- attemptProcess()
+    
+    if (result == FALSE) {
+      if (i == iteration) {
+        message("Final attempt failed. Exiting.")
+        break
+      } else {
+        message(paste0("An error occurred. Will retry in ", waitFor, " seconds..."))
+        Sys.sleep(waitFor)  # Wait for seconds before next attempt
+      }
+    } else {
+      message(paste("Process succeeded with result:", result))
+      break
+    }
+  }
 }
