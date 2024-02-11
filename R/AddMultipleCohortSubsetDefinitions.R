1b0cd8c2aa9a3c4e7405dfd88f5984e6e7608049
Gowtham Rao <rao@ohdsi.org>
2024-02-10 23:56:29 -0500

    k

--- R/AddMultipleCohortSubsetDefinitions.R
+++ R/AddMultipleCohortSubsetDefinitions.R
@@ -1,12 +1,13 @@
-#' Add Multiple Cohort Subset Definitions
+#' Add Multiple Cohort Subset Definitions with Progress Bar
 #'
 #' This function iteratively adds cohort subset definitions to a given cohort definition set.
 #' It processes multiple sequences of subset operators, first adding each operator from the first sequence individually,
 #' and then combining each operator from the first sequence with each operator from the subsequent sequences in turn.
+#' A progress bar and console messages indicate the progress of the operation.
 #'
 #' @param cohortDefinitionSet The initial cohort definition set to which subset definitions will be added.
 #' @param targetCohortIds A vector of target cohort IDs that the subset definitions will be applied to.
-#' @param prefix A text prefix for the cohort name.
+#' @param prefix A string prefix for the names of the subset definitions.
 #' @param ... Additional arguments representing sequences of subset operators.
 #' @return The modified cohort definition set with added subset definitions.
 #' @export
@@ -16,6 +17,7 @@
 #' # cohortDefinitionSetWithSubset <- addMultipleCohortSubsetDefinitions(
 #' #   cohortDefinitionSet,
 #' #   targetOrIndicationCohortIds,
+#' #   "CustomPrefix",
 #' #   subsetOperatorSequence1,
 #' #   subsetOperatorSequence2
 #' #   # ... Add more sequences if necessary
@@ -27,11 +29,17 @@ addMultipleCohortSubsetDefinitions <-
            ...) {
     # Extract the variable list of operator sequences.
     subsetOperatorSequences <- list(...)
+    totalDefinitions <-
+      Reduce("*", lapply(subsetOperatorSequences, length))
     definitionId <- 1
+    
+    # Initialize the progress bar
+    pb <- txtProgressBar(min = 0, max = totalDefinitions, style = 3)
     
     # Internal recursive function to add subset definitions.
     # currentOperators: A list of current operators to be included in the subset definition.
     # remainingSequences: A list of remaining operator sequences to be processed.
+    
     addSubsetDefinitions <-
       function(currentOperators, remainingSequences) {
         # Create and add a subset definition if currentOperators is not empty.
@@ -42,6 +50,14 @@ addMultipleCohortSubsetDefinitions <-
             subsetOperators = currentOperators
           )
           
+          message(
+            sprintf(
+              "Adding subset definition %d of %d: %s",
+              definitionId,
+              totalDefinitions,
+              subsetName
+            )
+          )
           cohortDefinitionSet <<-
             CohortGenerator::addCohortSubsetDefinition(
               cohortDefinitionSet,
@@ -49,6 +65,8 @@ addMultipleCohortSubsetDefinitions <-
               targetCohortIds = targetCohortIds
             )
           
+          # Update progress
+          setTxtProgressBar(pb, definitionId)
           definitionId <<- definitionId + 1
         }
         
@@ -64,6 +82,9 @@ addMultipleCohortSubsetDefinitions <-
     for (i in seq_along(subsetOperatorSequences)) {
       addSubsetDefinitions(list(), subsetOperatorSequences[i:length(subsetOperatorSequences)])
     }
+    
+    # Close the progress bar
+    close(pb)
     
     return(cohortDefinitionSet)
   }
