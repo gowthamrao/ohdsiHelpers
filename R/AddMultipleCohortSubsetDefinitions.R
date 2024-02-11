f136a8908ec74449a87d7e3c8a3babdc427dca80
Gowtham Rao <rao@ohdsi.org>
2024-02-10 19:11:07 -0500

    Create AddMultipleCohortSubsetDefinitions.R

---
+++ R/AddMultipleCohortSubsetDefinitions.R
@@ -0,0 +1,58 @@
+#' Add multiple cohort subset definitions
+#'
+#' This function adds multiple cohort subset definitions to a given cohort definition set.
+#' It iterates over provided sequences of subset operators in a nested manner,
+#' creating a unique subset definition for every possible combination of operators.
+#'
+#' @param cohortDefinitionSet A cohort definition set to which the subset definitions will be added.
+#' @param targetCohortIds A vector of target cohort IDs.
+#' @param ... One or more vectors of subset operators.
+#'
+#' @return The modified cohort definition set with added subset definitions.
+#'
+#' @examples
+#' cohortDefinitionSetWithSubset <- addMultipleCohortSubsetDefinitions(
+#'   cohortDefinitionSet,
+#'   targetOrIndicationCohortIds,
+#'   subsetOperatorSequence1,
+#'   subsetOperatorSequence2
+#'   # ... Add more sequences if necessary
+#' )
+addMultipleCohortSubsetDefinitions <-
+  function(cohortDefinitionSet,
+           targetCohortIds,
+           ...) {
+    subsetOperatorLists <- list(...)
+    definitionId <- 1
+    
+    # Internal recursive function to process subsets
+    processSubsets <- function(operators, currentOperators) {
+      if (length(operators) == 0) {
+        # Base case: no more operators to process, create and add subset definition
+        subsetDefinition <-
+          CohortGenerator::createCohortSubsetDefinition(
+            name = paste("Subset", definitionId),
+            definitionId = definitionId,
+            subsetOperators = currentOperators
+          )
+        
+        cohortDefinitionSet <<-
+          CohortGenerator::addCohortSubsetDefinition(
+            cohortDefinitionSet,
+            cohortSubsetDefintion = subsetDefinition,
+            targetCohortIds = targetCohortIds
+          )
+        
+        definitionId <<- definitionId + 1
+      } else {
+        # Recursive case: iterate through the current operator list and process further
+        for (operator in operators[[1]]) {
+          processSubsets(operators[-1], c(currentOperators, list(operator)))
+        }
+      }
+    }
+    
+    # Start the recursive process with the initial list of operator sequences
+    processSubsets(subsetOperatorLists, list())
+    return(cohortDefinitionSet)
+  }
