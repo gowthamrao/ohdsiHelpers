5d21c70ff30f35b9638108f0e77a4d9a8e4d795b
Gowtham Rao <rao@ohdsi.org>
2024-02-13 16:25:08 -0500

    e

--- R/RenderTranslateExecuteSqlInParallel.R
+++ R/RenderTranslateExecuteSqlInParallel.R
@@ -1,34 +1,46 @@
+#' Render, Translate, and Execute SQL Queries in Parallel Across Multiple CDM Sources
+#'
+#' This function is designed to execute SQL queries across multiple Common Data Model (CDM) sources in parallel.
+#' It filters the CDM sources based on specified criteria, renders and translates SQL queries for each source,
+#' and then executes them in parallel using a cluster of worker nodes.
+#'
+#' @param cdmSources A dataframe containing details of the CDM sources.
+#' @param sequence A sequence number to filter the CDM sources, defaults to 1.
+#' @param sql The SQL query string to be executed.
+#' @param tempEmulationSchema Temporary schema for SQL Render emulation, obtained from global option 'sqlRenderTempEmulationSchema' if not provided.
+#' @param databaseIds A vector of database IDs to filter the CDM sources, obtained from the getListOfDatabaseIds() function if not provided.
+#' @param userService The user service credential key, defaults to "OHDSI_USER".
+#' @param passwordService The password service credential key, defaults to "OHDSI_PASSWORD".
+#' @param ... Additional parameters that may be passed to the SQL rendering and translation functions.
 #' @export
-renderTranslateExecuteSqlInParallel <-
-  function(cdmSources,
-           sequence = 1,
-           sql,
-           tempEmulationSchema = getOption("sqlRenderTempEmulationSchema"),
-           databaseIds = getListOfDatabaseIds(),
-           userService = "OHDSI_USER",
-           passwordService = "OHDSI_PASSWORD",
-           ...) {
-    cdmSources <- cdmSources |>
-      dplyr::filter(.data$database %in% c(databaseIds)) |>
-      dplyr::filter(.data$sequence == !!sequence)
-    
-    x <- list()
-    for (i in 1:nrow(cdmSources)) {
-      x[[i]] <- cdmSources[i,]
-    }
-    
-    # use Parallel Logger to run in parallel
-    cluster <-
-      ParallelLogger::makeCluster(numberOfThreads = min(as.integer(trunc(
-        parallel::detectCores() /
-          2
-      )),
-      length(x)))
-    
-    renderTranslateExecuteSqlX <- function(x,
-                                           sql,
-                                           tempEmulationSchema,
-                                           ...) {
+renderTranslateExecuteSqlInParallel <- function(cdmSources,
+                                                sequence = 1,
+                                                sql,
+                                                tempEmulationSchema = getOption("sqlRenderTempEmulationSchema"),
+                                                databaseIds = getListOfDatabaseIds(),
+                                                userService = "OHDSI_USER",
+                                                passwordService = "OHDSI_PASSWORD",
+                                                ...) {
+  # Filter cdmSources based on provided database IDs and sequence number
+  cdmSources <- cdmSources |>
+    dplyr::filter(.data$database %in% c(databaseIds)) |>
+    dplyr::filter(.data$sequence == !!sequence)
+  
+  # Convert the filtered cdmSources to a list for parallel processing
+  x <- list()
+  for (i in 1:nrow(cdmSources)) {
+    x[[i]] <- cdmSources[i, ]
+  }
+  
+  # Initialize a cluster for parallel execution
+  cluster <- ParallelLogger::makeCluster(numberOfThreads = min(as.integer(trunc(
+    parallel::detectCores() / 2
+  )), length(x)))
+  
+  # Inner function to render, translate, and execute SQL for each CDM source
+  renderTranslateExecuteSqlX <-
+    function(x, sql, tempEmulationSchema, ...) {
+      # Create connection details for each CDM source
       connectionDetails <- DatabaseConnector::createConnectionDetails(
         dbms = x$dbms,
         user = keyring::key_get(userService),
@@ -36,8 +48,10 @@ renderTranslateExecuteSqlInParallel <-
         server = x$serverFinal,
         port = x$port
       )
-      connection = DatabaseConnector::connect(connectionDetails = connectionDetails)
+      connection <-
+        DatabaseConnector::connect(connectionDetails = connectionDetails)
       
+      # Render, translate, and execute SQL for the CDM source
       DatabaseConnector::renderTranslateExecuteSql(
         connection = connection,
         sql = sql,
@@ -49,16 +63,17 @@ renderTranslateExecuteSqlInParallel <-
         cdm_database_schema = x$cdmDatabaseSchemaFinal,
         ...
       )
-      
     }
-    ParallelLogger::clusterApply(
-      cluster = cluster,
-      x = x,
-      fun = renderTranslateExecuteSqlX,
-      sql = sql,
-      ...
-    )
-    
-    ParallelLogger::stopCluster(cluster = cluster)
-    
-  }
+  
+  # Apply the SQL execution function in parallel across the cluster
+  ParallelLogger::clusterApply(
+    cluster = cluster,
+    x = x,
+    fun = renderTranslateExecuteSqlX,
+    sql = sql,
+    ...
+  )
+  
+  # Stop the cluster after execution
+  ParallelLogger::stopCluster(cluster = cluster)
+}
