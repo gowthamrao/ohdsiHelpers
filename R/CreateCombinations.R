bfb1c0dffb5d26cb9747f14df5edce1c334c1866
Gowtham Rao <rao@ohdsi.org>
2024-02-14 07:46:45 -0500

    df

--- R/CreateCombinations.R
+++ R/CreateCombinations.R
@@ -4,7 +4,7 @@
 #' For each combination, it creates a tibble row for every element in the array that is part of that combination.
 #' The resulting tibble contains two columns: one for the individual elements and another for the combinations they appear in.
 #'
-#' @param array A character vector containing unique elements for which combinations are to be generated.
+#' @param array A vector containing unique elements for which combinations are to be generated.
 #'
 #' @return A tibble with two columns: `array` containing the individual elements, and `combinations` containing the combinations.
 #' @export
@@ -13,8 +13,12 @@ createCombinations <- function(array) {
   if (!requireNamespace("tibble", quietly = TRUE))
     install.packages("tibble")
   
+  # Convert array elements to character type for consistent processing
+  array <- as.character(array)
+  
   # Initialize an empty tibble to store combinations
-  resultDataFrame <- dplyr::tibble()
+  resultDataFrame <-
+    tibble::tibble(array = character(), combinations = character())
   
   # Loop over the length of the array to generate all possible combinations
   for (i in 1:length(array)) {
@@ -26,18 +30,14 @@ createCombinations <- function(array) {
     })
     
     # Loop over each combination
-    for (j in (1:length(sortedCombinations))) {
+    for (comb in sortedCombinations) {
       # Loop over each element in the array
       for (name in array) {
         # Check if the element is part of the current combination
-        if (name %in% unlist(strsplit(sortedCombinations[[j]], ","))) {
+        if (name %in% unlist(strsplit(comb, ","))) {
           # Add a row to the tibble with the element and its combination
-          resultDataFrame <-
-            dplyr::bind_rows(
-              resultDataFrame,
-              tibble::tibble(array = name, combinations = sortedCombinations[[j]])
-            ) |>
-            dplyr::distinct()
+          resultDataFrame <- dplyr::bind_rows(resultDataFrame,
+                                              tibble::tibble(array = name, combinations = comb))
         }
       }
     }
@@ -55,7 +55,8 @@ createCombinations <- function(array) {
     dplyr::arrange(id) |>
     dplyr::relocate(id,
                     combinations,
-                    name)
+                    array)
   
+  # Return the result data frame
   return(resultDataFrame)
 }
