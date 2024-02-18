edc76866e929ebd85671314fd7633671aba6ff07
Gowtham Rao <rao@ohdsi.org>
2024-02-18 07:34:39 -0500

    D

---
+++ R/DropCohortTables.R
@@ -0,0 +1,27 @@
+#' @export
+dropCohortTablesInParallel <- function(cdmSources,
+                                       cohortTableNames) {
+  sql <- "DROP TABLE IF EXSITS @cohort_database_schema.@cohort_table;
+
+          DROP TABLE IF EXSITS @cohort_database_schema.@cohort_inclusion_table;
+
+          DROP TABLE IF EXSITS @cohort_database_schema.@cohort_inclusion_result_table;
+
+          DROP TABLE IF EXSITS @cohort_database_schema.@cohort_inclusion_stats_table;
+
+          DROP TABLE IF EXSITS @cohort_database_schema.@cohort_summary_stats_table;
+
+          DROP TABLE IF EXSITS @cohort_database_schema.@cohort_censor_stats_table;"
+  
+  
+  OhdsiHelpers::renderTranslateExecuteSqlInParallel(
+    cdmSources = cdmSources,
+    sql = sql,
+    cohort_table = cohortTableNames$cohortTable,
+    cohort_inclusion_table = cohortTableNames$cohortInclusionTable,
+    cohort_inclusion_result_table = cohortTableNames$cohortInclusionResultTable,
+    cohort_inclusion_stats_table = cohortTableNames$cohortInclusionStatsTable,
+    cohort_summary_stats_table = cohortTableNames$cohortSummaryStatsTable,
+    cohort_censor_stats_table = cohortTableNames$cohortCensorStatsTable
+  )
+}
