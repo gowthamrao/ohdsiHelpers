c6b45a73ee99bc2d1f562b9bab84e1935f0ae04b
Gowtham Rao <rao@ohdsi.org>
2024-04-06 15:43:03 -0400

    Update GetTargetCohortToFeatureCohortDateDifferenceDistribution.R

--- R/GetTargetCohortToFeatureCohortDateDifferenceDistribution.R
+++ R/GetTargetCohortToFeatureCohortDateDifferenceDistribution.R
@@ -188,147 +188,192 @@ getTargetCohortToFeatureCohortDateDifferenceDistribution <-
     }
     
     sql <- "DROP TABLE IF EXISTS #target_cohort;
-          DROP TABLE IF EXISTS #feature_cohort;
-          DROP TABLE IF EXISTS #occurrence_next_date;
-          DROP TABLE IF EXISTS #days_to_next_start;
+            DROP TABLE IF EXISTS #feature_cohort;
+            DROP TABLE IF EXISTS #all_next_ft_date;
+            DROP TABLE IF EXISTS #days_to_next_cs_t;
+            DROP TABLE IF EXISTS #next_feature_date;
+            DROP TABLE IF EXISTS #agg_non_cum;
+            DROP TABLE IF EXISTS #agg_cum;
+            DROP TABLE IF EXISTS #aggregate;
+            
+            DROP TABLE IF EXISTS #days_to_next_start;
 
-      		SELECT subject_id,
-          			cohort_start_date,
-            		ROW_NUMBER() OVER (
-            			PARTITION BY subject_id
-            			ORDER BY cohort_start_date
-            			) AS start_sequence
-      	  INTO #target_cohort
-      		FROM (
-      		        SELECT DISTINCT subject_id, @target_cohort_anchor_date cohort_start_date
-      		        FROM @target_cohort_database_schema.@target_cohort_table_name
-      		        WHERE cohort_definition_id IN (@target_cohort_definition_ids)
-      		    ) a;
-
-        	SELECT DISTINCT subject_id, @feature_cohort_anchor_date cohort_start_date
-      	  INTO #feature_cohort
-		      FROM @feature_cohort_database_schema.@feature_cohort_table_name
-		      WHERE cohort_definition_id IN (@feature_cohort_definition_ids);
-
-          WITH next_feature_date as
-        	(
-          	SELECT DISTINCT t.subject_id,
-          		t.start_sequence,
-          		t.cohort_start_date,
-          		LEAD(f.cohort_start_date) OVER (
-          			PARTITION BY t.subject_id,
-          			t.cohort_start_date,
-          			t.start_sequence
-          			ORDER BY f.cohort_start_date
-          			) AS next_start_date
-          	FROM #target_cohort t
-          	INNER JOIN #feature_cohort AS f
-          		ON t.subject_id = f.subject_id
-          			AND t.cohort_start_date <= f.cohort_start_date
-          	),
-          	next_feature_date2 AS
-          	(
+            ------
+        		SELECT subject_id,
+            	cohort_start_date,
+              CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10)) event_id,
+            	ROW_NUMBER() OVER (
+            		PARTITION BY subject_id ORDER BY cohort_start_date
+            		) AS start_sequence
+            INTO #target_cohort
+            FROM (
+            	SELECT DISTINCT subject_id,
+            		@target_cohort_anchor_date cohort_start_date
+            	FROM @target_cohort_database_schema.@target_cohort_table_name
+            	WHERE cohort_definition_id IN (@target_cohort_definition_ids)
+            	) a;
+          
+            ------
+            
+            SELECT DISTINCT subject_id,
+            	@feature_cohort_anchor_date cohort_start_date
+            INTO #feature_cohort
+            FROM @feature_cohort_database_schema.@feature_cohort_table_name
+            WHERE cohort_definition_id IN (@feature_cohort_definition_ids);
+          
+            -----
+            SELECT subject_id,
+            	start_sequence,
+            	cohort_start_date,
+            	next_start_date,
+              DATEDIFF(day, cohort_start_date, next_start_date) AS days_to_next_cohort_start
+            INTO #all_next_ft_date
+            FROM (
+                	SELECT DISTINCT t.subject_id,
+                		t.start_sequence,
+                		t.cohort_start_date,
+                		LEAD(f.cohort_start_date) OVER (
+                			PARTITION BY t.subject_id,
+                			t.cohort_start_date ORDER BY f.cohort_start_date
+                			) AS next_start_date
+                	FROM #target_cohort t
+                	INNER JOIN #feature_cohort AS f ON t.subject_id = f.subject_id
+                		AND t.cohort_start_date <= f.cohort_start_date
+            	) f
+            WHERE next_start_date IS NOT NULL
+            -------------
+            ORDER BY subject_id,
+            	start_sequence,
+            	cohort_start_date,
+            	next_start_date;
+          
+            ------
+            with days_to_next_cohort_start_tm as
+              (
+                SELECT distinct days_to_next_cohort_start 
+                FROM #all_next_ft_date
+              )
+            select a.days_to_next_cohort_start days_to_next_cohort_start_group,
+                    b.days_to_next_cohort_start
+            INTO #days_to_next_cs_t
+            FROM days_to_next_cohort_start_tm a
+            CROSS JOIN
+                days_to_next_cohort_start_tm b
+             WHERE a.days_to_next_cohort_start >= b.days_to_next_cohort_start
+            ORDER BY a.days_to_next_cohort_start, b.days_to_next_cohort_start;
+              
+            ------
+            SELECT subject_id,
+              	cohort_start_date,
+              	start_sequence,
+              	next_start_date,
+              	CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10)) event_id,
+                DATEDIFF(day, cohort_start_date, next_start_date) AS days_to_next_cohort_start
+            INTO #next_feature_date
+            FROM
+            (
               SELECT subject_id,
-                      start_sequence,
-                      cohort_start_date,
-                      min(next_start_date) next_start_date
-              FROM next_feature_date
+              	start_sequence,
+              	cohort_start_date,
+              	min(next_start_date) next_start_date
+              FROM #all_next_ft_date
               GROUP BY subject_id,
-                      start_sequence,
-                      cohort_start_date
-          	)
-          	SELECT subject_id,
-          	        start_sequence,
-          	        cohort_start_date,
-          	        next_start_date,
-          	        DATEDIFF(day, cohort_start_date, next_start_date) AS days_to_next_cohort_start,
-                    CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10)) AS event_id,
-                    DENSE_RANK() OVER (PARTITION BY start_sequence
-                                        ORDER BY subject_id) AS subject_rank,
-                    DENSE_RANK() OVER (PARTITION BY start_sequence
-                                        ORDER BY CAST(subject_id AS VARCHAR(25)
-                                        ) || '-' || CAST(next_start_date AS VARCHAR(10))) AS event_rank
-            INTO #occurrence_next_date
-          	FROM next_feature_date2
-          	WHERE next_start_date IS NOT NULL
-          ;
-
+              	start_sequence,
+              	cohort_start_date
+            ) f
+            ORDER BY subject_id,
+              	cohort_start_date,
+              	start_sequence,
+              	next_start_date;
+            
+            ------
+            SELECT start_sequence,
+                  days_to_next_cohort_start,
+                  COUNT(DISTINCT subject_id) AS subjects,
+                  COUNT(DISTINCT event_id) AS events
+            INTO #agg_non_cum
+            FROM #next_feature_date f 
+            GROUP BY start_sequence,
+                  days_to_next_cohort_start;
+          
+            ------
+            SELECT start_sequence,
+                  days_to_next_cohort_start_group days_to_next_cohort_start,
+                  COUNT(DISTINCT subject_id) AS subjects_cum
+            INTO #agg_cum
+            FROM #next_feature_date nf
+            INNER JOIN
+                #days_to_next_cs_t ss
+            ON nf.days_to_next_cohort_start = ss.days_to_next_cohort_start
+            GROUP BY start_sequence,
+                  days_to_next_cohort_start_group;
+          
+            ------
+            SELECT anc.start_sequence,
+                    anc.days_to_next_cohort_start,
+                    subjects,
+                    events,
+                    subjects_cum subjects_cumulative
+            INTO #aggregated
+            FROM #agg_non_cum anc
+            INNER JOIN #agg_cum ac
+            ON anc.start_sequence = ac.start_sequence AND
+              anc.days_to_next_cohort_start = ac.days_to_next_cohort_start
+            ORDER BY anc.start_sequence,
+                    anc.days_to_next_cohort_start;
+          
+            ------
+          
           SELECT *
           INTO #days_to_next_start
-          FROM
-          (
-            SELECT -2 AS start_sequence,
-                  -2 AS days_to_next_cohort_start,
-                	count(DISTINCT subject_id) subjects,
-                	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #target_cohort
-
-            UNION
-
-            SELECT start_sequence,
-                  -2 AS days_to_next_cohort_start,
-                	count(DISTINCT subject_id) subjects,
-                	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #target_cohort
-            GROUP BY start_sequence
-
-            UNION
-
-            SELECT -1 AS start_sequence,
-                  -1 AS days_to_next_cohort_start,
-                	count(DISTINCT subject_id) subjects,
-                	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #occurrence_next_date
-
-            UNION
-
-            SELECT start_sequence,
-                  -1 AS days_to_next_cohort_start,
-                	count(DISTINCT subject_id) subjects,
-                	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #occurrence_next_date
-            GROUP BY start_sequence
-
-            UNION
-
-            SELECT -1 start_sequence,
-                  days_to_next_cohort_start,
-                	count(DISTINCT subject_id) subjects,
-                	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #occurrence_next_date
-            GROUP BY days_to_next_cohort_start
-
-            UNION
-
-            SELECT start_sequence,
-            	days_to_next_cohort_start,
-            	count(DISTINCT subject_id) subjects,
-            	count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10))) events,
-                	MAX(subject_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS subjects_cumulative,
-                  MAX(event_rank) OVER (ORDER BY start_sequence ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS events_cumulative
-            FROM #occurrence_next_date
-            WHERE next_start_date IS NOT NULL
-            GROUP BY start_sequence,
-            	days_to_next_cohort_start
-          ) f
+          FROM (
+          	SELECT - 2 AS start_sequence,
+          		- 2 AS days_to_next_cohort_start,
+          		count(DISTINCT subject_id) AS subjects,
+          		count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10))) AS events,
+          		count(DISTINCT subject_id) AS subjects_cumulative
+          	FROM #target_cohort
+          	
+          	UNION
+          	SELECT start_sequence,
+          		- 2 AS days_to_next_cohort_start,
+          		count(DISTINCT subject_id) AS subjects,
+          		count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(cohort_start_date AS VARCHAR(10))) AS events,
+          		count(DISTINCT subject_id) AS subjects_cumulative
+          	FROM #target_cohort
+          	GROUP BY start_sequence
+          	
+          	UNION
+          	
+          	SELECT - 1 AS start_sequence,
+          		- 1 AS days_to_next_cohort_start,
+          		count(DISTINCT subject_id) subjects,
+          		count(DISTINCT CAST(subject_id AS VARCHAR(25)) || '-' || CAST(next_start_date AS VARCHAR(10))) events,
+          		count(DISTINCT subject_id) subjects_cumulative
+          	FROM #all_next_ft_date
+          	
+          	UNION
+          	
+          	SELECT start_sequence,
+          		days_to_next_cohort_start,
+          		subjects,
+          		events,
+          		subjects_cumulative
+          	FROM #aggregated
+          	) f
           ORDER BY start_sequence,
-                    days_to_next_cohort_start,
-                    subjects,
-                    events;
+          	days_to_next_cohort_start,
+          	subjects,
+          	events;
 
           DROP TABLE IF EXISTS #target_cohort;
           DROP TABLE IF EXISTS #feature_cohort;
-          DROP TABLE IF EXISTS #occurrence_next_date;
+          DROP TABLE IF EXISTS #all_next_ft_date;
+          DROP TABLE IF EXISTS #days_to_next_cs_t;
+          DROP TABLE IF EXISTS #next_feature_date;
+          DROP TABLE IF EXISTS #agg_non_cum;
+          DROP TABLE IF EXISTS #agg_cum;
+          DROP TABLE IF EXISTS #aggregate;
 "
     DatabaseConnector::renderTranslateExecuteSql(
       connection = connection,
