7c0957c1423abb9f655ddef06e6dc287fa67152b
Gowtham Rao <rao@ohdsi.org>
2024-04-06 20:17:03 -0400

    d

---
+++ R/CalculateSummaryStatistics.R
@@ -0,0 +1,35 @@
+#' @export
+calculateSummaryStatistics <-
+  function(df, value = "value", group = "group") {
+    # Ensure correct package references
+    dataFrame <- df |>
+      dplyr::mutate(!!group := .data[[group]],!!value := .data[[value]]) |>
+      dplyr::select(.data[[group]],
+                    .data[[value]])
+    
+    # Helper function to calculate mode
+    calculateMode <- function(x) {
+      ux <- unique(x)
+      ux[which.max(tabulate(match(x, ux)))]
+    }
+    
+    output <- dataFrame |>
+      dplyr::group_by(.data[[group]]) |>
+      dplyr::summarize(
+        mean = mean(.data[[value]], na.rm = TRUE),
+        sd = sd(.data[[value]], na.rm = TRUE),
+        median = median(.data[[value]], na.rm = TRUE),
+        p5 = quantile(.data[[value]], 0.05, na.rm = TRUE),
+        p25 = quantile(.data[[value]], 0.25, na.rm = TRUE),
+        p75 = quantile(.data[[value]], 0.75, na.rm = TRUE),
+        p95 = quantile(.data[[value]], 0.95, na.rm = TRUE),
+        mode = calculateMode(.data[[value]])
+      ) |>
+      tidyr::pivot_longer(
+        cols = -all_of(group),
+        names_to = "statistic",
+        values_to = "value"
+      )
+    
+    return(output)
+  }
--- R/EditCohortGeneratorIncrementalLog.R
+++ R/EditCohortGeneratorIncrementalLog.R
@@ -1,3 +1,33 @@
+#' @export
+editCohortGeneratorIncrementalLogInParallel <-
+  function(incrementalFolder,
+           incrementalFileName = "GeneratedCohorts.csv",
+           deleteCohortIds = NULL,
+           keepCohortIds = NULL) {
+    incrementalFiles <- list.files(
+      path = incrementalFolder,
+      pattern = incrementalFileName,
+      all.files = TRUE,
+      full.names = TRUE,
+      recursive = TRUE,
+      ignore.case = TRUE
+    )
+    
+    if (length(incrementalFiles) == 0) {
+      stop("no incremental files found")
+    }
+    
+    for (i in (1:length(incrementalFiles))) {
+      OhdsiHelpers::editCohortGeneratorIncrementalLog(
+        incrementalFolder = dirname(incrementalFiles[[i]]),
+        incrementalFileName = basename(incrementalFiles[[i]]),
+        keepCohortIds = cohortIdsToKeep
+      )
+    }
+  }
+
+
+
 #' Edit Incremental Cohort Generator Log
 #'
 #' This function edits a CSV file containing information about cohorts,
@@ -10,12 +40,12 @@
 #'        edited. Defaults to "GeneratedCohorts.csv".
 #' @param deleteCohortIds A numeric vector of cohort IDs to be removed from the
 #'        log file. If NULL, no deletions are made. Defaults to NULL.
-#'
-#' @export
 #' @return The function does not return anything, but it updates the specified
 #'         CSV file in the given folder.
 #' @examples
 #' editCohortGeneratorIncrementalLog("path/to/folder", deleteCohortIds = c(101, 102))
+#'
+#' @export
 editCohortGeneratorIncrementalLog <- function(incrementalFolder,
                                               incrementalFileName = "GeneratedCohorts.csv",
                                               deleteCohortIds = NULL,
@@ -26,29 +56,28 @@ editCohortGeneratorIncrementalLog <- function(incrementalFolder,
       file = file.path(incrementalFolder, incrementalFileName),
       col_types = readr::cols()
     )
-
+  
   convertToDateTime <- function(dataFrame, columnName) {
-    dataFrame[[columnName]] <- as.POSIXct(dataFrame[[columnName]], format = "%Y/%m/%d %H:%M:%S")
+    dataFrame[[columnName]] <-
+      as.POSIXct(dataFrame[[columnName]], format = "%Y/%m/%d %H:%M:%S")
     return(dataFrame)
   }
-
-  cohortGenerator <- convertToDateTime(
-    dataFrame = cohortGenerator,
-    columnName = "timeStamp"
-  )
-
+  
+  cohortGenerator <- convertToDateTime(dataFrame = cohortGenerator,
+                                       columnName = "timeStamp")
+  
   # If there are cohort IDs specified for deletion, filter them out
   if (!is.null(deleteCohortIds)) {
     cohortGenerator <- cohortGenerator |>
       dplyr::filter(!.data$cohortId %in% deleteCohortIds)
   }
-
+  
   # If there are cohort IDs specified for keep
   if (!is.null(keepCohortIds)) {
     cohortGenerator <- cohortGenerator |>
       dplyr::filter(.data$cohortId %in% keepCohortIds)
   }
-
+  
   # Write the updated data frame back to the CSV file
   readr::write_csv(
     x = cohortGenerator,
